07ddaf0a90a5e47c4229ddec1d0993d61ffe6b33
Claudio Spizzi <claudio@spizzi.net>
2025-09-16 23:08:44 +0200

    Update build script

---
+++ .build.ps1
@@ -0,0 +1,7 @@
+﻿
+# Import build tasks
+. InvokeBuildHelperTasks
+
+# # Build configuration
+# $IBHConfig.RepositoryTask.TokenCallback = { Use-VaultSecureString -TargetName 'GitHub Token (claudiospizzi)' }
+# $IBHConfig.GalleryTask.TokenCallback    = { Use-VaultSecureString -TargetName 'PowerShell Gallery Key (claudiospizzi)' }
